b308cc85f99136656e759fe513e443e61961439d
x240 <v-defender@ukr.net>
2015-06-05 00:03:20 +0200

    add simple test for HomeController

---
+++ src/test/java/com/elstele/bill/test/controller/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package com.elstele.bill.test.controller;
+
+/**
+ * Created by X240 on 04/06/2015.
+ */
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/test/resources/test-servlet-context.xml")
+public class HelloControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void indexPageResponseTest() throws Exception {
+        this.mockMvc.perform(get("/")
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk());
+    }
+
+}
