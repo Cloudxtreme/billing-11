35a0f26daa5257f42eb5b583d2e74011d2122af2
Ivan Shaposhnikov <v-defender@ukr.net>
2015-09-24 23:44:48 +0200

    - add workerPOC with executor and controller

---
+++ src/main/java/com/elstele/bill/executors/WorkerPOC.java
@@ -0,0 +1,39 @@
+package com.elstele.bill.executors;
+
+/**
+ * Created by X240 on 24/09/2015.
+ */
+public class WorkerPOC implements Runnable{
+
+    private int taskId;
+    private int progress;
+
+    public WorkerPOC(int taskId) {
+        this.taskId = taskId;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while(progress != 100) {
+                Thread.sleep(1000);
+                progressIncrease();
+                System.out.println("Task id:" + taskId + " progress:" + progress);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private synchronized void progressIncrease(){
+        progress = progress+1;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+}
