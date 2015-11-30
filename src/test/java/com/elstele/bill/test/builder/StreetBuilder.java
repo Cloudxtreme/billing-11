77bd1eed4e18f7e445ff8e4d14a101c338b5841d
ivan <v-defender@ukr.net>
2015-11-30 22:40:52 +0100

    FEATURE: add test objects builders

---
+++ src/test/java/com/elstele/bill/test/builder/StreetBuilder.java
@@ -0,0 +1,39 @@
+package com.elstele.bill.test.builder;
+
+import com.elstele.bill.domain.Street;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+/**
+ * Created by ivan on 15/11/30.
+ */
+public class StreetBuilder implements TestObjectCreator<StreetBuilder, Street>{
+
+    private SecureRandom random = new SecureRandom();
+    private Street street;
+
+    public StreetBuilder build(){
+        street = new Street();
+        return this;
+    }
+
+    public StreetBuilder withName(String name){
+        street.setName(name);
+        return this;
+    }
+
+    public StreetBuilder withRandomName(){
+        String name = new BigInteger(130, random).toString(32);
+        street.setName(name);
+        return this;
+    }
+
+    public Street getRes(){
+        if (street == null){
+            this.build();
+        }
+        return street;
+    }
+
+}
