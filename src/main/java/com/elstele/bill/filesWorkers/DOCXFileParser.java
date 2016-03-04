f0ba9fc9504af536da20f82ef93e221bb15fb8d6
lamo3abp <alexandrkaskov@gmail.com>
2016-03-04 15:21:06 +0200

    getText from table rows

--- src/main/java/com/elstele/bill/filesWorkers/DOCXFileParser.java
+++ src/main/java/com/elstele/bill/filesWorkers/DOCXFileParser.java
@@ -4,8 +4,10 @@ import com.elstele.bill.utils.Enums.ResponseToAjax;
 import com.elstele.bill.utils.LocalDirPathProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +18,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 @Service
 public class DOCXFileParser {
@@ -33,8 +36,16 @@ public class DOCXFileParser {
         try {
             FileInputStream fis = new FileInputStream(file);
             XWPFDocument doc = new XWPFDocument(fis);
-            XWPFWordExtractor extract = new XWPFWordExtractor(doc);
-            LOGGER.info(extract.getText());
+            List<XWPFTable> tables=  doc.getTables();
+            for(XWPFTable table : tables){
+                List<XWPFTableRow> rowList = table.getRows();
+                for(XWPFTableRow row : rowList){
+                    List<XWPFTableCell> cellList =  row.getTableCells();
+                    for(XWPFTableCell cell : cellList){
+                        System.out.println(cell.getText());
+                    }
+                }
+            }
         }catch(IOException e){
             LOGGER.error(e.getMessage() ,e);
         }finally {
